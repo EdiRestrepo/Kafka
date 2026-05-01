20c54e3e5274fdd9207f45f7cf736916369c697f
EdiRestrepo <edisonestival@gmail.com>
2026-04-30 21:00:29 -0500

    refactor: replace field injection with constructor injection for ProductService in ProductCommandConsumer

--- products-command/src/main/java/com/springcloud/kafka/command/handlers/ProductCommandConsumer.java
+++ products-command/src/main/java/com/springcloud/kafka/command/handlers/ProductCommandConsumer.java
@@ -6,7 +6,6 @@ import com.springcloud.kafka.command.models.dto.ProductDto;
 import com.springcloud.kafka.command.services.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +14,11 @@ import java.util.function.Consumer;
 @Configuration
 public class ProductCommandConsumer {
     private static final Logger log = LoggerFactory.getLogger(ProductCommandConsumer.class);
+    private final ProductService productService;
 
-    @Autowired
-    private ProductService productService;
+    public ProductCommandConsumer(ProductService productService) {
+        this.productService = productService;
+    }
 
     @Bean
     public Consumer<Command<ProductDto>> handleCommands() {
